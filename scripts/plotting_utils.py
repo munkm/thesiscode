ba41365ff5ac7c8a994d21d4a6354c93e1bb7b0d
Madicken Munk <madicken.munk@gmail.com>
2017-03-21 11:49:55 -0700

    WIP: adding plots by group, stripplot and violin, still not classed

--- scripts/plotting_utils.py
+++ scripts/plotting_utils.py
@@ -14,6 +14,15 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 ###############################################################################
 
+# Default Values for various things
+
+names = [u'adjoint_anisotropy', u'forward_anisotropy', u'metric_five',
+u'metric_four', u'metric_one', u'metric_six', u'metric_three', u'metric_two']
+
+
+#-----------------------------------------------------------------------------#
+
+
 def energy_histogram(energy_bound, tally_result, title='default title',
         color='#412966', lowest_bin=1e-10):
     if len(energy_bound) == len(tally_result):
@@ -36,9 +45,49 @@ def energy_histogram(energy_bound, tally_result, title='default title',
     plt.ylabel('Relative Error')
     plt.xscale('log')
     plt.savefig('/Users/madicken/Documents/wwdebug/sillyplot.png', hbox_inches='tight')
-    # plt.show()
-    plt.close()
 
+def violinbyenergy(data, plot_title, x_title, y_title, savepath):
+    plot_title = str(plot_title)
+    x_title = str(x_title)
+    y_title = str(y_title)
+
+    sns.set_style("whitegrid")
+    plt.figure(figsize=(15,5))
+    pal = sns.diverging_palette(10, 240, n=27)
+    sns.violinplot(data=data, palette=pal, bw=.2, cut=0, linewidth=1, log_bins=True)
+    plt.title(plot_title)
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.yscale('log')
+    plt.savefig('%s' %(savepath), hbox_inches='tight')
+
+def violinbygroup(data, plot_title, x_title, x_names, y_title, savepath):
+    sns.set_style("whitegrid")
+    plt.figure(figsize=(10,5))
+    plt.yscale('log')
+    pal=sns.color_palette("YlOrRd", 16)
+    sns.violinplot(data=data[:,1:], palette=pal[8:], linewidth=1, cut=0,
+                   scale='width', gridsize=100, inner='box', log_bins=True,
+                   bw=.2)
+    plt.title(plot_title)
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.xticks(np.arange(len(names)),names, rotation=45)
+    plt.gcf().subplots_adjust(bottom=0.20)
+    plt.savefig('%s' %(savepath), hbox_inches='tight')
+
+def stripbygroup(data, plot_title, x_title, x_names, y_title, savepath):
+    sns.set_style("whitegrid")
+    plt.figure(figsize=(10,5))
+    pal=sns.color_palette("YlOrRd", 16)
+    plt.yscale('log')
+    sns.stripplot(data=data, palette=pal[8:], jitter=True, size=1)
+    plt.title(plot_title)
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.xticks(np.arange(len(names)), names, rotation=45)
+    plt.gcf().subplots_adjust(bottom=0.20)
+    plt.savefig('%s' %(savepath), hbox_inches='tight')
 
 
 #-----------------------------------------------------------------------------#
