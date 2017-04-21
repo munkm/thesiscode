82bef27d029f7bde6b9ef81ed1233d807d2e54ec
Madicken Munk <madicken.munk@gmail.com>
2017-04-21 16:43:10 -0700

    added pandas functionality for FOM tables in compare_runs

--- scripts/compare_runs.py
+++ scripts/compare_runs.py
@@ -15,6 +15,7 @@ from plotting_utils import energy_histogram, styles
 import logging
 from matplotlib import pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 ###############################################################################
 
@@ -38,16 +39,17 @@ class Compare_Runs(object):
 
         return data
 
-    def plot_tally_result(self):
+    def plot_tally_result(self, savepath=None):
         self.plot_compare(compare_type='tallied_result',
                 y_label='Tally Result', title='%s comparison of tally result'
-                %self.problem_name)
+                %self.problem_name, savepath=savepath)
         return
 
-    def plot_tally_error(self):
+    def plot_tally_error(self, savepath=None):
         self.plot_compare(compare_type='relative_error',
                 y_label='Tally Relative Error',
-                title='%s comparison of tally relative error' %self.problem_name)
+                title='%s comparison of tally relative error'
+                %self.problem_name, savepath=savepath)
         return
 
     def plot_compare(self, compare_type='tallied_result',
@@ -87,10 +89,15 @@ class Compare_Runs(object):
             return fig
         return
 
-    def make_fom_table(self):
-        cad = self.cadisdata.frames['fom_frame'].transpose()
-        cadom = self.cadisangledata.frames['fom_frame'].transpose()
-        analog = self.cadisangledata.frames['fom_frame'].transpose()
+    def make_table(self, framename):
+        print('composite frame of %s being created' %framename)
+
+        cad = self.cadisdata.frames[framename].transpose()
+        cad_label = self.cadisdata.method_type
+        cadom = self.cadisangledata.frames[framename].transpose()
+        cadom_label = self.cadisangledata.method_type
+        analog = self.analogdata.frames[framename].transpose()
+        analog_label = self.analogdata.method_type
 
         alldata = [cad, cadom, analog]
         keys = [cad_label, cadom_label, analog_label]
@@ -100,29 +107,52 @@ class Compare_Runs(object):
 
         return newtable
 
-    def make_tallytable(self):
-
-        return
-
     def do_compare_analysis(self, plot_tally_results=False, plot_tally_error=False,
-            make_fom_table=False):
+            make_fomtable=False, make_tallytable=False):
 
         if plot_tally_results == True:
-            if problem_name:
-                savepath = self.analysis_dir+'/%s_tally_result_compare.png' %problem_name
+            if self.problem_name:
+                savepath = self.analysis_dir+'/%s_tally_result_compare.png' \
+                           %self.problem_name
             else:
                 savepath = self.analysis_dir+'/tally_result_compare.png'
 
             self.plot_tally_result(savepath=savepath)
 
         if plot_tally_error == True:
-            self.plot_tally_error()
+            if self.problem_name:
+                savepath = self.analysis_dir+'/%s_tally_error_compare.png' \
+                           %self.problem_name
+            else:
+                savepath = self.analysis_dir+'/tally_error_compare.png'
 
-        if make_fom_table == True:
-            self.make_fom_table()
+            self.plot_tally_error(savepath=savepath)
+
+        if make_fomtable == True:
+            table = self.make_table('fom_frame')
+
+            if self.problem_name:
+                savepath = self.analysis_dir+'/%s_tally_foms_compare.txt' \
+                           %self.problem_name
+            else:
+                savepath = self.analysis_dir+'/tally_foms_compare.txt'
+
+            table = table.to_string(float_format='%.2f')
+            with open(savepath, 'w') as fp:
+                fp.write(table)
 
         if make_tallytable == True:
-            self.make_tally_table()
+            table = self.make_table('tally_frame')
+
+            if self.problem_name:
+                savepath = self.analysis_dir+'/%s_tally_converge_compare.txt' \
+                           %self.problem_name
+            else:
+                savepath = self.analysis_dir+'/tally_converge_compare.txt'
+
+            table = table.to_string()
+            with open(savepath, 'w') as fp:
+                fp.write(table)
 
 ###############################################################################
 # end of thesiscode/scripts/compare_runs.py
